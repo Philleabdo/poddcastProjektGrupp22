1b54d068b4863378cb374fcd3246b0ee87d355e9
P-P-R <paulus.razai.petrus@gmail.com>
2025-11-19 15:11:20 +0100

    Första designen Form1
    .

--- poddcastProjektGrupp22/Form1.Designer.cs
+++ poddcastProjektGrupp22/Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.buttonNyhetsskalla = new Button();
+            this.listBoxInformation = new ListBox();
+            this.listBoxLankar = new ListBox();
+            label1 = new Label();
+            this.buttonSpara = new Button();
+            buttonVisning = new Button();
+            textBoxURL = new TextBox();
+            SuspendLayout();
+            // 
+            // buttonNyhetskalla
+            // 
+            this.buttonNyhetsskalla.Location = new Point(12, 56);
+            this.buttonNyhetsskalla.Name = "buttonNyhetskalla";
+            this.buttonNyhetsskalla.Size = new Size(200, 23);
+            this.buttonNyhetsskalla.TabIndex = 0;
+            this.buttonNyhetsskalla.Text = "Hämta och visa nyhetskälla";
+            this.buttonNyhetsskalla.UseVisualStyleBackColor = true;
+            // 
+            // listBoxInformation
+            // 
+            this.listBoxInformation.FormattingEnabled = true;
+            this.listBoxInformation.Location = new Point(12, 105);
+            this.listBoxInformation.Name = "listBoxInformation";
+            this.listBoxInformation.Size = new Size(309, 304);
+            this.listBoxInformation.TabIndex = 1;
+            // 
+            // listBoxLankar
+            // 
+            this.listBoxLankar.FormattingEnabled = true;
+            this.listBoxLankar.Location = new Point(340, 105);
+            this.listBoxLankar.Name = "listBoxLankar";
+            this.listBoxLankar.Size = new Size(332, 304);
+            this.listBoxLankar.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Ange URL:";
+            // 
+            // buttonSpara
+            // 
+            this.buttonSpara.Location = new Point(12, 415);
+            this.buttonSpara.Name = "buttonSpara";
+            this.buttonSpara.Size = new Size(75, 23);
+            this.buttonSpara.TabIndex = 4;
+            this.buttonSpara.Text = "Spara";
+            this.buttonSpara.UseVisualStyleBackColor = true;
+            // 
+            // buttonVisning
+            // 
+            buttonVisning.Location = new Point(93, 415);
+            buttonVisning.Name = "buttonVisning";
+            buttonVisning.Size = new Size(142, 23);
+            buttonVisning.TabIndex = 5;
+            buttonVisning.Text = "Ange visningsnamn";
+            buttonVisning.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBoxURL.Location = new Point(12, 27);
+            textBoxURL.Name = "textBox1";
+            textBoxURL.Size = new Size(357, 23);
+            textBoxURL.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(textBoxURL);
+            Controls.Add(buttonVisning);
+            Controls.Add(this.buttonSpara);
+            Controls.Add(label1);
+            Controls.Add(this.listBoxLankar);
+            Controls.Add(this.listBoxInformation);
+            Controls.Add(this.buttonNyhetsskalla);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonNyhetsskalla;
+        private ListBox listBoxLankar;
+        private ListBox listBoxInformation;
+        private Label label1;
+        private Button buttonSpara;
+        private Button buttonVisning;
+        private TextBox textBoxURL;
     }
 }
