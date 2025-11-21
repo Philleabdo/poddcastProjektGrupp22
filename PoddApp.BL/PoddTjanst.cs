baef6693802cbc97aad40795c315972a07f556e6
P-P-R <paulus.razai.petrus@gmail.com>
2025-11-21 15:29:01 +0100

    PoddTjanst
    .

--- PoddApp.BL/PoddTjanst.cs
+++ PoddApp.BL/PoddTjanst.cs
@@ -1,10 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using PoddApp.Models;
 
 namespace PoddApp.BL
 {
-    internal class PoddTjanst
+    public class PoddTjanst
     {
+        //Tillfälligt register (byter till MongoDB senare)
+        private List<Poddflode> _poddar = new List<Poddflode>();
+        private List<Avsnitt> _avsnitt = new List<Avsnitt>();
+
+        public void SparaNyPodd(string visningsNamn, string rssUrl, List<Avsnitt> avsnitt)
+        {
+            //Spara ny podd + dess avsnitt
+            string nyttId = Guid.NewGuid().ToString();
+
+            //Skapa poddobjekt
+            Poddflode podd = new Poddflode
+            {
+                Id = nyttId,
+                Namn = visningsNamn,
+                RssUrl = rssUrl,
+                SkapaDatum = DateTime.Now
+            };
+            //Lägg till i listan
+            _poddar.Add(podd);
+            //Koppla varje avsnitt till podden och spara dem
+            foreach (var a in avsnitt)
+            {
+                a.PoddflodeId = nyttId;
+                _avsnitt.Add(a);
+            }
+        }
+        //Hämta alla sparade poddar (till Form2 vänster lista)
+        public List<Poddflode> HamtaAllaPoddar()
+        {
+            return _poddar;
+        }
+        //Hämtar alla avsnitt som tillhör en viss podd (så Form 2 kan visa)
+        public List<Avsnitt> HamtaAvsnittForPodd(string poddId)
+        {
+            List<Avsnitt> resultat = new List<Avsnitt>();
+
+            foreach (var a in _avsnitt)
+            {
+                if (a.PoddflodeId == poddId)
+                    resultat.Add(a);
+            }
+
+            return resultat;
+        }
     }
 }
