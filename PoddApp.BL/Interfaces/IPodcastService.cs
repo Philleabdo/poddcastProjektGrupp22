4c0c1721919c9cb600850a0217178039c41ffe1b
kyri-Irakoze <kyri.irakoze@gmail.com>
2025-11-21 14:36:22 +0100

    update

---
+++ PoddApp.BL/Interfaces/IPodcastService.cs
@@ -0,0 +1,12 @@
+﻿using PoddApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoddApp.BL.Interfaces
+{
+    public interface IPodcastService
+    {
+        //Task<List
+    }
+}
