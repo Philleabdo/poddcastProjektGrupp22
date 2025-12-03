751081aae7e48e67fcd44915b5257d26de964b3e
kyri-Irakoze <kyri.irakoze@gmail.com>
2025-12-03 17:42:48 +0100

    skapat IPoddRepository.cs

---
+++ PoddApp.DAL/Interface/IPoddRepository.cs
@@ -0,0 +1,15 @@
+﻿using PoddApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoddApp.DAL.Interface
+{
+    public interface IPoddRepository
+    {
+        Task SparaNyPoddAsync(Poddflode podd, List<Avsnitt> avsnitt);
+        Task<List<Poddflode>> HamtaAllaPoddarAsync();
+        Task<List<Avsnitt>> HamtaAvsnittForPoddAsync(string poddId);
+        Task TaBortPoddAsync(string poddId);
+    }
+}
